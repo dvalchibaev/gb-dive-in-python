cf2b6ce7ba041a73cef4baa03aeeb270de4fc508
dvalchibaev <alchibaevdv@gmail.com>
2023-05-23 12:23:57 +0300

    lesson10 practice

--- lesson08/dir-handler-pkg/create_data.py
+++ lesson08/dir-handler-pkg/create_data.py
@@ -1,7 +1,23 @@
-from random import randint, random, choices
+from random import randint, random, choices, choice
 from string import ascii_letters
 import os
 
 
-def create_data() -> bool:пше
+def create_data(n: int = 12) -> bool:
+    TYPES = ('dir', 'byte')
+    os.mkdir('TEST')
+    for _ in range(n):
+        name = ''.join(choices(ascii_letters, k=randint(6, 10)))
+        file_type = choice(TYPES)
+        if file_type == 'dir':
+            os.mkdir(f'TEST/{name}')
+            print(name)
+        else:
+            print(f'file: {name}')
     return True
+
+
+if __name__ == "__main__":
+    create_data()
+
+
